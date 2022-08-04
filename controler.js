2d8e3f44619ef8f3e3f9b51da2c05d0d0e9cba4e
rakinplaban <rakinshahriar54@gmail.com>
2022-08-04 18:28:35 +0600

    click event listener works fine on the button.

--- controler.js
+++ controler.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("Dom fully loaded!");
 
-    document.querySelector("#button_h5").addEventListener("onClick",ramen);
+    document.querySelector("#button_h5").addEventListener("click",ramen);
 })
 
 
 function ramen(){
     console.log("Button Works!");
-    document.querySelector("#button_h5").innerHTML = "Hi, pappy!";
+    document.querySelector("#pipper").innerHTML = "Hi, pappy!";
 }
