016bc6797fb17dd56e3ad17b36bcf628436acac6
rakinplaban <rakinshahriar54@gmail.com>
2022-08-09 20:16:40 +0600

    loop updated

--- controler.js
+++ controler.js
@@ -3,13 +3,17 @@ twits = ["This worldy life is very short.","This worldy life is nothing but an i
 document.addEventListener("DOMContentLoaded",()=>{
     console.log("Dom fully loaded!");
     let counter = 0
-    for(let i =0 ;i<twits.length;i++){
-        document.querySelector("#button_h5").addEventListener("click",()=>{
-            document.querySelector("#pipper").innerHTML = twits[i];
-        });
+    // for(let i =0 ;i<twits.length;i++){
+    //     document.querySelector("#button_h5").addEventListener("click",()=>{
+    //         document.querySelector("#pipper").innerHTML = twits[i];
+    //     });
+    //     counter++;
+    //     console.log(counter," Button works!")
+    // }
+    document.querySelector("#button_h5").addEventListener('click', ()=>{
         counter++;
-        console.log(counter," Button works!")
-    }
+        console.log(counter,"Button works!");
+    })
     // document.querySelector("#button_h5").addEventListener("click",ramen);
 })
 
